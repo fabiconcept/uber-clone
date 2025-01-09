eb23bb5d8cfb5c7aa1958ea8173845021ba690ba
fabiconcept <favourajokubi@gmail.com>
2025-01-09 17:40:10 +0100

    Design confirm-ride & find-ride pages

--- app/(root)/find-ride.tsx
+++ app/(root)/find-ride.tsx
@@ -1,4 +1,9 @@
+import CustomButton from '@/components/CustomButton';
+import GoogleTextInput from '@/components/GoogleTextInput';
+import RideLayout from '@/components/RideLayout';
+import { icons } from '@/constants';
 import { useLocationStore } from '@/store';
+import { router } from 'expo-router';
 import { View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -10,13 +15,43 @@ export default function findRide() {
         setDestinationLocation
     } = useLocationStore();
     return (
-        <SafeAreaView>
-            <Text className='text-2xl'>
-                You are here: {userAddress}
-            </Text>
-            <Text className='text-2xl'>
-                You are going to: {destinationAddress}
-            </Text>
-        </SafeAreaView>
+        <RideLayout
+            title={`Ride`}
+            snapPoints={['85%']}
+        >
+            <View className='my-3'>
+                <Text className='text-lg font-JakartaSemiBold mb-3'>
+                    From
+                </Text>
+                <GoogleTextInput
+                    icon={icons.target}
+                    initialLocation={userAddress!}
+                    containerStyle='bg-neutral-100'
+                    textInputBackgroundColor='#f5f5f5'
+                    handlePress={(location) => {
+                        setUserLocation(location);
+                    }}
+                />
+            </View>
+            <View className='my-3'>
+                <Text className='text-lg font-JakartaSemiBold mb-3'>
+                    To
+                </Text>
+                <GoogleTextInput
+                    icon={icons.map}
+                    initialLocation={destinationAddress!}
+                    containerStyle='bg-neutral-100'
+                    textInputBackgroundColor='transparent'
+                    handlePress={(location) => {
+                        setDestinationLocation(location);
+                    }}
+                />
+            </View>
+            <CustomButton
+                title="Find Now"
+                onPress={() => router.push("/(root)/confirm-ride")}
+                className='mt-5'
+            />
+        </RideLayout>
     )
 }
