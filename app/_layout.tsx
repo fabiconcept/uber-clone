42182f6380c5e72a3c5b3c850565171ad81dcc82
fabiconcept <favourajokubi@gmail.com>
2025-01-12 13:29:33 +0100

    Update app/_layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,13 +3,20 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { Fragment, useEffect } from 'react';
+import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { LogBox } from "react-native";
 import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
 import { tokenCache } from "@/Lib/Auth";
+import { StripeProvider } from '@stripe/stripe-react-native';
+
+LogBox.ignoreLogs([
+  "Clerk:",
+]);
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+const stripePublishableKey = process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY!
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -17,6 +24,14 @@ export default function RootLayout() {
     throw new Error(
       'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
     )
+  }
+
+  if (!stripePublishableKey) {
+    throw new Error(
+      'Missing Stripe Key. Please set EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY in your .env',
+    )
+  } else {
+    console.log(stripePublishableKey)
   }
 
   const [loaded] = useFonts({
@@ -46,12 +61,14 @@ export default function RootLayout() {
   return (
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
       <ClerkLoaded>
-        <Stack>
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="(root)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
+        <StripeProvider publishableKey={stripePublishableKey}>
+          <Stack>
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="(root)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+        </StripeProvider>
         <StatusBar style="dark" />
       </ClerkLoaded>
     </ClerkProvider>
